37d4ff376dfc8a3a136a4d132764e1180bb7efd9
DamZyl <damianzylka@outlook.com>
2021-04-26 17:11:20 +0200

    Add swagger

--- src/PlainClasses.ApiGateway/Startup.cs
+++ src/PlainClasses.ApiGateway/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -18,20 +10,20 @@ namespace PlainClasses.ApiGateway
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerForOcelot(Configuration);
+            services.AddControllers();
             services.AddOcelot();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,8 +31,8 @@ namespace PlainClasses.ApiGateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
+            app.UseSwaggerForOcelotUI(Configuration);
+                
             app.UseRouting();
 
             app.UseAuthorization();
